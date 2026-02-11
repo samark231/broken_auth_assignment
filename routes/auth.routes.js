aa04b8887808a5cf82f7c9f1e53eb547505b7e88
samar Krishna <amitkumar231677@gmail.com>
2026-02-12 00:27:30 +0530

    CHORES: adding comments where i havemade the fixes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const  { handleLogin, handleToken, handleVerifyOtp } =  require('../controllers/auth.controllers');
 const router = express.Router();
 
+//just to check if auth req are reaching authRoutes
 router.get("/", (req,res)=>{
     res.json({
         message: "auth is working"
